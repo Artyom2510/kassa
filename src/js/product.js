f3b18475850bdbff820de0a6608f5f6933d8d4c0
Maxim <shevkoplias.mk@yandex.ru>
2020-06-12 11:08:59 +0300

    попапы

--- src/js/product.js
+++ src/js/product.js
@@ -64,11 +64,35 @@ $(function () {
 		prevScroll = st;
 	});
 
+	function updateProductPopup() {
+		var $checked = $('.js-product-props:checked');
+		var $checkedParent = $checked.parent('.checked-dop');
+		var checkedId = $checked.val();
+		var checkedName = $checkedParent.find('.checked-dop__desc').text();
+		var checkedPrice = $checkedParent.find('.checked-dop__price').text();
+
+		$(
+			'.popup-product-device__input.popup-product-device__input_additional'
+		).val(checkedId);
+
+		$('.popup-product-device__right > span:first-child').text(checkedName);
+
+		if (checkedPrice) {
+			$('.popup-product-device__right > span:last-child').text(checkedPrice);
+		} else {
+			$('.popup-product-device__right > span:last-child').text('0 руб.');
+		}
+	}
+
+	updateProductPopup();
+
 	$('.js-product-props').on('change', function () {
 		var curVal = $(this).val();
 		$('.js-product-price').each(function (idx, el) {
 			$(el).text(Number($(el).data('start')) + Number(curVal));
 		});
+
+		updateProductPopup();
 	});
 
 	$('.js-goto-form').on('click', function () {
